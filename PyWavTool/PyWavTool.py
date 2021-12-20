0d40210e95d3521da33d44e65b5ff5722d9895ea
delta-kimigatame <sankaku.kuro@gmail.com>
2021-12-20 20:50:53 +0900

    docstringを日本語で再整理、入力値のチェックを__init__から__inputCheckに分離

--- PyWavTool/PyWavTool.py
+++ PyWavTool/PyWavTool.py
@@ -1,40 +1,48 @@
 
+import os
 import os.path
 import wave
 import argparse
+
+import whd
 
 ARROW_ENVELOPE_VALUES = [2, 8, 9, 10]
 
 
 class WavTool:
     '''
-    WavTool is emulate UTAU's wavtool.
-    input wav to output wavheader(output.whd) and wavbody(output.wbd)
+    WavToolはUTAU標準のwavtoolをエミュレートします。
+    wavを入力し、waveヘッダ(.wav.whd)とwaveデータ(.wav.dat)を出力します。
+    すでにwhdとdatがある場合、datの末尾にデータを加え、whdを更新します。
 
     Attributes
     ----------
     ourput : str
-        output wav path
+        出力するwavのパス
     input : str
-        input wav path
+        入力するwavのパス
     stp : float
-        start offset of wav
+        入力wavの先頭のオフセットをmsで指定する。
     length : float
-        append ms
+        datに追加する長さ(ms)
     envelope : list
-        envelope patern is
+        エンベロープのパターンは以下のいずれかです。
             p1 p2
             p1 p2 p3 v1 v2 v3 v4 ove
             p1 p2 p3 v1 v2 v3 v4
             p1 p2 p3 v1 v2 v3 v4 ove p4 p5 v5
         p1,p2,p3,p4,p5,ove : float
         v1,v2,v3,v4,v5 : int
+    header : whd.Whd
+        .whdファイルを扱います。
     '''
     __error: bool = False
     __output: str
     __input: str
     __stp: float
     __length: float
+    __data: list
+    __header: whd.Whd
 
     def __init__(self,
                  output: str,
@@ -46,15 +54,15 @@ class WavTool:
         Parameters
         ----------
         ourput : str
-            output wav path
+            出力するwavのパス
         input : str
-            input wav path
+            入力するwavのパス
         stp : float
-            start offset of wav
+            入力wavの先頭のオフセットをmsで指定する。
         length : float
-            append ms
+            datに追加する長さ(ms)
         envelope : list
-            envelope patern is
+            エンベロープのパターンは以下のいずれかです。
                 p1 p2
                 p1 p2 p3 v1 v2 v3 v4 ove
                 p1 p2 p3 v1 v2 v3 v4
@@ -62,13 +70,46 @@ class WavTool:
             p1,p2,p3,p4,p5,ove : float
             v1,v2,v3,v4,v5 : int
         '''
-        if (len(args.envelope) not in ARROW_ENVELOPE_VALUES):
-            # is amount of envelope values matching patern?
+        self.__inputCheck(output,input,envelope)
+        os.makedirs(os.path.split(output)[0], exist_ok=True)
+        self.__header = whd.Whd(output + ".whd")
+
+    def __inputCheck(self,output,input,envelope):
+        '''
+        入力値が正しいかチェックします。
+        正常値の場合、self.__dataにwavの中身を代入します。
+        異常値の場合、self.__errorをTrueにします。
+        Parameters
+        ----------
+        ourput : str
+            出力するwavのパス
+        input : str
+            入力するwavのパス
+        envelope : list
+            エンベロープのパターンは以下のいずれかです。
+                p1 p2
+                p1 p2 p3 v1 v2 v3 v4 ove
+                p1 p2 p3 v1 v2 v3 v4
+                p1 p2 p3 v1 v2 v3 v4 ove p4 p5 v5
+            p1,p2,p3,p4,p5,ove : float
+            v1,v2,v3,v4,v5 : int
+        '''
+        if (len(envelope) not in ARROW_ENVELOPE_VALUES):
+            # エンベロープがパターンにマッチしているか確認
             print("value error:envelope patern is not matching.")
             self.__error = True
         if (not os.path.isfile(input)):
             print("input file is not found:{}".format(input))
             self.__error = True
+        try:
+            with wave.open(input,"rb") as wr:
+                self.__data=wr.readframes(wr.getnframes())
+        except:
+            print("file format error:{} is not wave.".format(input))
+            self.__error = True
+
+
+
         
 
 

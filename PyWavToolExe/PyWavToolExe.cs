3a8d632260b6460efe6b40ea1ea3a1abcf699a00
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-20 15:13:39 +0900

    エンドポイントの作成

---
+++ PyWavToolExe/PyWavToolExe.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+internal class PyWavToolExe
+{
+    static void Main(string[] args)
+    {
+        String python = ".\\python-3.9.10\\python.exe";
+        String endpoint = ".\\src\\PyWavTool.py";
+        Process p = Process.Start(python, endpoint + " " + String.Join(" ", args));
+        p.WaitForExit();
+    }
+}

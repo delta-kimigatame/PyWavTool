459120745779c05d12a19f67a2228d858861397e
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-20 10:27:21 +0900

    エンベロープ適用までの処理を作成

--- PyWavTool/PyWavTool.py
+++ PyWavTool/PyWavTool.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import wave
 import argparse
+from typing import Tuple
 
 import whd
 
@@ -43,6 +44,8 @@ class WavTool:
     _stp: float
     _length: float
     _data: list
+    _stp_data :list
+    _apply_data :list
     _header: whd.Whd
 
     @property
@@ -76,19 +79,37 @@ class WavTool:
             p1,p2,p3,p4,p5,ove : float
             v1,v2,v3,v4,v5 : int
         '''
-        self._inputCheck(output,input,envelope)
+        ove :float
+        self._error = False
+        self._inputCheck(input, envelope)
         os.makedirs(os.path.split(output)[0], exist_ok=True)
         self._header = whd.Whd(output + ".whd")
+        if len(envelope) >= 8:
+            ove = envelope[7]
+        else:
+            ove = 0
 
-    def _inputCheck(self,output,input,envelope):
+        if not self._error:
+            addframes :int = int((length-ove) * self._header.framerate / 1000)
+            self._ApplyStp(stp)
+            p, v = self._GetEnvelopes(envelope, length)
+        #**todo**
+        #dataにエンベロープを適用する。
+        #エンベロープポイントがframeとmsが一致しないとき、単純にエンベロープの数字との大小で適用を判断する
+        #nframe - addのfrmae数文は、既存のdatの値に足す
+        #のこりのframe数はdatの末尾に追加
+        #datがない場合oveは無視する。
+        #datが足りない場合は足りない分だけ音が前にずれる。(GUI側でオーバーラップの最大を前のノート長に制限)
+
+
+    def _inputCheck(self, input:str ,envelope: list):
         '''
-        入力値が正しいかチェックします。
-        正常値の場合、self._dataにwavの中身を代入します。
-        異常値の場合、self._errorをTrueにします。
+        | 入力値が正しいかチェックします。
+        | 正常値の場合、self._dataにwavの中身を最大1に正規化したfloatに変換して代入します。
+        | 異常値の場合、self._errorをTrueにします。
+
         Parameters
         ----------
-        ourput : str
-            出力するwavのパス
         input : str
             入力するwavのパス
         envelope : list
@@ -101,20 +122,118 @@ class WavTool:
             p1,p2,p3,p4,p5,ove : float
             v1,v2,v3,v4,v5 : int
         '''
+
+        basedata :list
+        data :list =[]
+        self._data = []
         if (len(envelope) not in ARROW_ENVELOPE_VALUES):
             # エンベロープがパターンにマッチしているか確認
             print("value error:envelope patern is not matching.")
             self._error = True
+            return
         if (not os.path.isfile(input)):
             print("input file is not found:{}".format(input))
             self._error = True
+            return
         try:
             with wave.open(input,"rb") as wr:
-                self._data=wr.readframes(wr.getnframes())
+                basedata = wr.readframes(wr.getnframes())
+                samplewidth :int = wr.getsampwidth()
         except:
             print("file format error:{} is not wave.".format(input))
             self._error = True
+            return
+        for i in range(int(len(basedata)/samplewidth)):#byteからintに変換。
+            data.append(int.from_bytes(basedata[i*samplewidth:(i+1)*samplewidth], 'little', signed=True))
+        self._data = list(map(lambda i:i / (2 ** (samplewidth * 8) /2), data)) #正規化
 
+    def _ApplyStp(self, stp:float):
+        '''
+        | stpを適用し、self._stp_dataを返します。
+        | 事前にself._dataに最大1に正規化したwavデータが格納されていることが条件です。
+
+        Parameters
+        ----------
+        stp : float
+            入力wavの先頭のオフセットをmsで指定する。
+        '''
+        stp_frames = int(stp * self._header.framerate / 1000)
+        self._stp_data = self._data[stp_frames:]
+
+    def _ApplyEnvelope(self, p :list, v :list):
+        '''
+        | エンベロープ・stpを適用し、self._apply_dataを返します。
+        | 事前にself._stp_dataに最大1に正規化したwavデータが格納されていることが条件です。
+
+        Parameters
+        ----------
+        p :list of float
+            Pstart P1 P2 P3 (P5) P4 Pendの順に並べたポルタメント。エンベロープが2点の場合空配列
+        v: list of int
+            ノート頭からms順に並べたポルタメントの音量値。エンベロープが2点の場合空配列
+        '''
+
+        if len(p) == 0: #休符等の例外
+            self._apply_data = [0] * len(self._stp_data)
+            return
+
+        self._apply_data = []
+        pos: int = 0
+        delta:float = (v[pos+1] - v[pos]) / (p[pos+1] - p[pos])
+        for i in range(len(self._stp_data)):
+            if i >= p[pos + 1]:
+                pos = pos +1
+                delta = (v[pos+1] - v[pos]) / (p[pos+1] - p[pos])
+            self._applv_data.append([pos] + delta * (i - p[pos]))
+
+    def _GetEnvelopes(self, envelope: list, length: float) -> Tuple[list, list]:
+        '''
+        | エンベロープをノート頭からのms順に並べ、pとvのリストを返します。
+        | エンベロープがパターンにマッチすることを事前に確認するのが条件です。
+        Parameters
+        ----------
+        envelope : list
+            エンベロープのパターンは以下のいずれかです。
+                p1 p2
+                p1 p2 p3 v1 v2 v3 v4
+                p1 p2 p3 v1 v2 v3 v4 ove
+                p1 p2 p3 v1 v2 v3 v4 ove p4
+                p1 p2 p3 v1 v2 v3 v4 ove p4 p5 v5
+            p1,p2,p3,p4,p5,ove : float
+            v1,v2,v3,v4,v5 : int
+        length : float
+            datに追加する長さ(ms)
+
+        Returns
+        -------
+        p :list of int
+            Pstart P1 P2 P3 (P5) P4 Pendの順に並べたポルタメント。エンベロープが2点の場合空配列
+        v: list of int
+            ノート頭からms順に並べたポルタメントの音量値。エンベロープが2点の場合空配列
+        '''
+        
+        if len(envelope) == 2: #休符等の例外
+            return [], []
+
+        p :list = [0] #Pstart
+        v :list = [0] #Vstart
+        frame_per_ms: float = self._header.framerate / 1000
+        p.append(int(float(envelope[0]) * frame_per_ms)) #p1
+        p.append(int(float(envelope[1]) * frame_per_ms)) #p2
+        p.append(int(float(envelope[2]) * frame_per_ms)) #p3
+        v.append(int(envelope[3])) #v1
+        v.append(int(envelope[4])) #v2
+        v.append(int(envelope[5])) #v3
+        if len(envelope) >= 11:
+            p.append(int((length - float(envelope[8]) - float(envelope[9])) * frame_per_ms)) #p5 はp4からのms
+            v.append(int(envelope[10])) #v5
+        v.append(int(envelope[6])) #v4
+        if len(envelope) >= 9:
+            p.append(int((length - float(envelope[8])) * frame_per_ms)) #p4は後ろからのms
+        p.append(length * frame_per_ms) # Pend
+        v.append(0) #Vend
+
+        return p,v
 
 
         

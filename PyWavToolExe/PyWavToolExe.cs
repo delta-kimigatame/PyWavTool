526aa1fbc189c03bd278cdcf52b61415f5b604c1
delta_kimigatame <sankaku.kuro@gmail.com>
2022-03-03 22:12:05 +0900

    処理時間計測用のコードをコメントアウト

--- PyWavToolExe/PyWavToolExe.cs
+++ PyWavToolExe/PyWavToolExe.cs
@@ -4,11 +4,13 @@ internal class PyWavToolExe
 {
     static void Main(string[] args)
     {
+        //Console.WriteLine(DateTime.Now.Millisecond);
         string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
         String python = Path.Combine(Path.GetDirectoryName(exePath), "python-3.9.10", "python.exe");
         String endpoint = Path.Combine(Path.GetDirectoryName(exePath), "src", "PyWavTool.py");
-        //Process p = Process.Start(python, endpoint + " " + String.Join(" ", args));
-        Process p = Process.Start(python, "-m cProfile "+endpoint + " " + String.Join(" ", args));
+        Process p = Process.Start(python, endpoint + " " + String.Join(" ", args));
+        //Process p = Process.Start(python, "-m cProfile "+endpoint + " " + String.Join(" ", args));
         p.WaitForExit();
+        //Console.WriteLine(DateTime.Now.Millisecond);
     }
 }
